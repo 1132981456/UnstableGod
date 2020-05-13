be4749bc7685991bee6435d870af9fa084e807e8
hukai <532809319@qq.com>
2020-05-13 18:49:39 +0800

    增加说明

--- 乘法精度.js
+++ 乘法精度.js
@@ -1,5 +1,20 @@
+/**
+ * 由于计算机由 1,0 翻译导致计算机出现精度的问题
+ * 目前以下方法可以解决该精度问题
+ * @param num
+ * @param decimal
+ */
+
+function mathRound(num, decimal) {
+    if (isNaN(num)) {
+        return 0;
+    }
+    const p1 = Math.pow(10, decimal + 1);
+    const p2 = Math.pow(10, decimal);
+    return Math.round(num * p1 / 10) / p2;
+}
 function MathToFixed(num, decimal) {
-    return round(num, decimal).toFixed(decimal);
+    return mathRound(num, decimal).toFixed(decimal);
 }
 function accMul(arg1,arg2){
     var m=0,s1=arg1.toString(),s2=arg2.toString();
@@ -15,4 +30,7 @@ function accMul(arg1,arg2){
     }
     return Number(s1.replace(".",""))*Number(s2.replace(".","")) / Math.pow(10,m)
 }
-console.log(parseFloat(MathToFixed(accMul(num1,num2),2)))
+
+//示例
+let num1=0.85,num2=0.009;
+console.log(parseFloat(MathToFixed(accMul(num1,num2),2)));
